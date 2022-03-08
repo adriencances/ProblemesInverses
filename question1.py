057092af3a6a3177f1b8f40f258f7b609ff176db
adriencances <adriencances@gmail.com>
2022-03-08 14:40:59 +0100

    essai ifft

--- question1.py
+++ question1.py
@@ -1,5 +1,6 @@
 import numpy as np
 np.random.seed(0)
+from numpy.fft import ifft
 from numpy.linalg import norm
 from tqdm import tqdm
 import matplotlib.pyplot as plt
@@ -37,7 +38,7 @@ def plot_ys(ys):
     return
 
 
-omega = np.linspace(-3,3,100)
+omega = np.linspace(-3,3,101)
 ys = np.random.multivariate_normal(mu ,cov, N)
 with open("q1/ys.npy", "wb") as f:
     np.save(f, ys)
@@ -69,6 +70,31 @@ def C_N(tau, x1, x2):
     res /= 2*np.pi*N
     return np.real(res)
 
+def C_N_ifft(x1, x2):
+    C_N_vals = np.zeros(omega.shape)
+    for y in tqdm(ys):
+        f_val = F_hat(omega)
+        g_val1 = G_hat(omega, x1, y)
+        g_val2 = G_hat(omega, x2, y)
+        assert f_val.shape == g_val1.shape
+        assert f_val.shape == g_val2.shape
+        fourier_vals = f_val*np.conj(g_val1)*g_val2
+        fourier_vals = np.roll(fourier_vals, -len(omega)//2)
+        int_vals = np.real(ifft(fourier_vals))
+        C_N_vals += int_vals
+    C_N_vals /= 2*np.pi*N
+    return np.real(C_N_vals)
+
+
+xs = get_xs(case=2)
+x1 = xs[1]
+x2 = xs[0]
+C_N_vals = C_N_ifft(x1, x2)
+plt.figure()
+plt.plot(C_N_vals)
+plt.savefig("Essai_C_N_x2_x1.png")
+
+
 
 # for case in [1, 2, 3]:
 #     xs = get_xs(case)
@@ -85,20 +111,20 @@ def C_N(tau, x1, x2):
 #         plt.ylabel(r"$C_N(\tau,x_{j+1},x_1)$")
 #         plt.savefig(f"q1/case{case}/plot_x{j+1}x1.png")
 
-for case in [2]:
-    xs = get_xs(case)
-    taus = np.linspace(-10,10,100)
-    C_N_vals = [dict() for j in range(5)]
-    for j in range(5):
-        for tau in tqdm(taus):
-            C_N_vals[j][tau] = C_N(tau, xs[j], xs[0])
-        # with open(f"q1/case{case}/C_N_x{j+1}.npy", "wb") as f:
-        #     np.save(f, C_N_vals[j])
-        plt.figure()
-        plt.plot(C_N_vals[j].keys(), C_N_vals[j].values())
-        plt.xlabel(r"$\tau$")
-        plt.ylabel(r"$C_N(\tau,x_{},x_1)$".format(j+1))
-        plt.savefig(f"q1/case{case}_c0_2/plot_x{j+1}x1.png")
+# for case in [2]:
+#     xs = get_xs(case)
+#     taus = np.linspace(-10,10,100)
+#     C_N_vals = [dict() for j in range(5)]
+#     for j in range(5):
+#         for tau in tqdm(taus):
+#             C_N_vals[j][tau] = C_N(tau, xs[j], xs[0])
+#         # with open(f"q1/case{case}/C_N_x{j+1}.npy", "wb") as f:
+#         #     np.save(f, C_N_vals[j])
+#         plt.figure()
+#         plt.plot(C_N_vals[j].keys(), C_N_vals[j].values())
+#         plt.xlabel(r"$\tau$")
+#         plt.ylabel(r"$C_N(\tau,x_{},x_1)$".format(j+1))
+#         plt.savefig(f"q1/case{case}_c0_2/plot_x{j+1}x1.png")
 
 
 # plot_F_hat()
